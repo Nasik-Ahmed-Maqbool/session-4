4df1ebfaba554e0f5ae3b403fa445a416dfdbf05
Nasik-Ahmed-Maqbool <ahmednasik085@gmail.com>
2020-09-13 01:40:24 +0500

    react app for session 4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [count , setcount] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h1>That's the value of count: {count}</h1>
+     {/* <button onClick={()=>alert("Button Presses")}>update counter</button>  */}
+     <button onClick={()=>setcount(count + 1)}>update counter</button>
     </div>
   );
 }
