466af5897398d1bf90613a48db25a516ce220324
Nasik-Ahmed-Maqbool <ahmednasik085@gmail.com>
2020-09-14 01:41:18 +0500

    updated session4 created message.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
+import {Message} from './Message.js'
 import './App.css';
 
 function App() {
   let [count , setcount] = useState(1);
+  let [ismorning , setmorning] = useState(false);
 
   return (
-    <div>
-     <h1>That's the value of count: {count}</h1>
+    <div className={`box ${ismorning ? 'daylight':''}`}>
+      <h2>Have a Good = {ismorning ? 'Morning':'Night'}</h2>
+      <Message counter={count} />
+     {/* <h1>That's the value of count: {count}</h1> */}
      {/* <button onClick={()=>alert("Button Presses")}>update counter</button>  */}
      <button onClick={()=>setcount(count + 1)}>update counter</button>
+     <button onClick={()=>setmorning(!ismorning)}>update day</button>
     </div>
   );
 }
---
+++ src/Message.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Message(props){
+return(
+
+    <h3>value of counter variable is {props.counter}</h3>
+);
+
+
+}
